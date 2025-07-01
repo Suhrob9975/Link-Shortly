08d7b17afcd80b94c3be8c450c2517eba21cea64
Suhrob <qurbonmurodvsuhrob4@gmail.com>
2025-07-02 02:37:30 +0500

    Co-authored-by: @Suhrob9975 <qurbonmurodovsuhrob4@gmail.com> Co-authored-by: @SuhrobQurbonmurodov <qurbonmurodovsuhrob293@gmail.com>

--- src/main/java/uz/pdp/linkshortly/repository/UrlMappingRepository.java
+++ src/main/java/uz/pdp/linkshortly/repository/UrlMappingRepository.java
@@ -10,5 +10,7 @@ import java.util.Optional;
  */
 
 public interface UrlMappingRepository extends JpaRepository<UrlMapping, Long> {
+
     Optional<UrlMapping> findByShortCode(String shortCode);
+
 }
